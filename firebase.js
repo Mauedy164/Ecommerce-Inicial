75f4bfd77d8d1aec62bc8439f5919ae2c359b707
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-02-04 23:13:05 -0600

    Añade conectividad con firestore

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-9IDTLWs9pbVJ-uEGNIKCqV55j2sG8tU",
+  authDomain: "react-pagina-marcelo.firebaseapp.com",
+  projectId: "react-pagina-marcelo",
+  storageBucket: "react-pagina-marcelo.appspot.com",
+  messagingSenderId: "857573718587",
+  appId: "1:857573718587:web:abc1f1ee0768881ac0ba1e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, getDocs };
--- src/components/CardProducts/CardProductsList.jsx
+++ src/components/CardProducts/CardProductsList.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { CardProduct } from "./CardProducts";
-import serviciosFisioterapia from '../../../data';
-import '../../styles/CardProductsList.css'
+import { db, collection, getDocs } from '../../../firebase';
+import '../../styles/CardProductsList.css';
 
 export function CardProductsList() {
     const [searchParams] = useSearchParams();
     const category = searchParams.get('category');
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        const fetchServices = async () => {
+            const querySnapshot = await getDocs(collection(db, "Productos"));
+            const servicesData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setServices(servicesData);
+        };
+
+        fetchServices();
+    }, []);
 
     const filteredServices = category
-        ? serviciosFisioterapia.filter(service => service.category === category)
-        : serviciosFisioterapia;
+        ? services.filter(service => service.category === category)
+        : services;
 
     const list = filteredServices.map((product) => (
         <CardProduct
